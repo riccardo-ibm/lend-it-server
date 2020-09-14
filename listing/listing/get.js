e7d3aa36625b0d52de3ab78be101e203cb6edaeb
Riccardo Trebbi <riccardotrebbi@Riccardos-MacBook-Pro.local>
2020-09-14 11:58:46 +0800

    Added CORS to get API

--- listing/listing/get.js
+++ listing/listing/get.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const { createErrorResponse, createSuccessResponse } = require('./utils');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
@@ -17,19 +18,12 @@ module.exports.get = (event, context, callback) => {
         // handle potential errors
         if (error) {
             console.error(error);
-            callback(null, {
-                statusCode: error.statusCode || 501,
-                headers: { 'Content-Type': 'text/plain' },
-                body: 'Couldn\'t fetch the todo item.',
-            });
+            callback(null, createErrorResponse(error.statusCode || 501, `Couldn't fetch the listing`))
             return;
         }
 
         // create a response
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify(result.Item),
-        };
+        const response = createSuccessResponse(result.Item);
         callback(null, response);
     });
 };
