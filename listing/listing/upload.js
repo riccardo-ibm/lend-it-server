5dd716e91c0cc08c5d3ca4f9eb6ef7403948ee59
Riccardo Trebbi <riccardotrebbi@Riccardos-MacBook-Pro.local>
2020-09-14 17:58:26 +0800

    Added resize image when uploading to S3

--- listing/listing/upload.js
+++ listing/listing/upload.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const sharp = require('sharp');
 const uuid = require('uuid');
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 const FileType = require('file-type');
@@ -12,23 +13,29 @@ module.exports.upload = (event, context, callback) => {
     const imageBuffer = Buffer.from(data.base64Image, 'base64');
     FileType.fromBuffer(imageBuffer).then(res => {
         const fileName = `${uuid.v1()}.${res.ext}`;
-        s3.upload({
-            Bucket: process.env.IMAGE_BUCKET,
-            Key: fileName,
-            Body: imageBuffer,
-        }, (err, response) => {
-            console.log(response);
-            if (err) {
-                callback(null, createErrorResponse(err.statusCode, `Couldn't upload the file`));
-            }
-            else {
-                callback(null, createSuccessResponse({
-                    fileUrl: response.Location
-                }))
-            }
-        });
+        sharp(imageBuffer)
+            .resize(400, 400, {
+                fit: 'contain',
+            })
+            .toBuffer()
+            .then(function (outputBuffer) {
+                s3.upload({
+                    Bucket: process.env.IMAGE_BUCKET,
+                    Key: fileName,
+                    Body: outputBuffer,
+                }, (err, response) => {
+                    console.log(response);
+                    if (err) {
+                        callback(null, createErrorResponse(err.statusCode, `Couldn't upload the file`));
+                    }
+                    else {
+                        callback(null, createSuccessResponse({
+                            fileUrl: response.Location
+                        }))
+                    }
+                });
+            });
     }, err => {
         callback(null, createErrorResponse(err.statusCode, `An error has occurred while determining the file type`));
     })
-
 }
